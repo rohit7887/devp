58d9782dfb3d44e597efad512d1ed0f41ce70930
Rohit R <47154150+rohit7887@users.noreply.github.com>
2021-12-04 00:38:43 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "key_name" {
+  default = "rohit"
+}
+
+variable "pvt_key_name" {
+  default = "/root/.ssh/my-key.pem"
+}
+
+
+variable "sg_id" {
+  default = "sg-088d65be8591f3c50"
+}
+
+
+variable "ami_id" {
+  default = "ami-05803413c51f242b7"
+}
+
+
+variable "region" {
+  default = "us-east-2"
+}
+
