3c230359e6c9903e917bc00605b2fa1c61cca6c2
Rohit R <47154150+rohit7887@users.noreply.github.com>
2021-12-04 01:13:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,67 @@
-resource "aws_instance" "backend" {
-  ami               = var.ami_id
-  instance_type     = "t2.micro"
-  key_name          = var.key_name
-  vpc_security_group_ids = [var.sg_id]
-  lifecycle {
-    prevent_destroy = false
-  }
-  tags = {
-    Name = "Dev-App"
-  }
+provider "aws" {
 
-  connection { 
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file(var.pvt_key_name)
-    host  = self.public_ip 
-  }
+  region  = "us-east-2"
 
-  
-  provisioner "remote-exec" {
-    inline = [
-      "sudo sleep 30",
-      "sudo apt-get update -y",
-      "sudo apt-get install python sshpass -y"
-    ]
+  version = ">=3.7, <=3.11"
 
-  }
+}
+
+
+
+variable "zones" {
+
+  default = ["us-east-2a", "us-east-2b"]
+
 }
 
 
 
 
 
+
+
+resource "aws_instance" "frontend" {
+
+  ami           = "ami-0dd0ccab7e2801812"
+
+  count         = 2
+
+  instance_type = "t2.micro"
+
+  availability_zone = var.zones[count.index]  
+
+  depends_on = [aws_instance.backend]
+
+  tags = {
+
+    Name = "Frontend-${count.index +1}"
+
+  }
+
+}
+
+
+
+
+
+
+
+resource "aws_instance" "backend" {
+
+  ami           = "ami-0dd0ccab7e2801812"
+
+  count         = 2
+
+  instance_type = "t2.micro"
+
+  availability_zone = var.zones[count.index]  
+
+
+
+  tags = {
+
+    Name = "Backend-${count.index +1}"
+
+  }
+
+}
