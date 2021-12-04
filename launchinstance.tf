9e31bad4d10e0dd714004c7ee1e15c939c3b55cd
root <root@ubu.wlgvm22hd3sejatfz3wj0mfxsf.cx.internal.cloudapp.net>
2021-12-04 03:08:18 +0000

    lab commit 4

--- launchinstance.tf
+++ launchinstance.tf
@@ -9,16 +9,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0dd0ccab7e2801812"
   instance_type = "t2.micro"
-  key_name          = var.key_name
-  vpc_security_group_ids = [var.sg_id]
   tags = {
     Name = "Launched-via-Terraform"
   }
  
-  connection { 
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file(var.pvt_key_name)
-    host  = self.public_ip 
-  }
 }
