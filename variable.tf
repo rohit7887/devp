87ca0149b0f434df428fb060a6de71ceffd4068f
Rohit R <47154150+rohit7887@users.noreply.github.com>
2021-12-04 08:30:48 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "key_name" {
+  default = "terraform-demo-mt-key-pair"
+}
+
+variable "pvt_key_name" {
+  default = "/root/.ssh/my-key.pem"
+}
+
+
+variable "sg_id" {
+  default = "sg-066a3fa7da661d24e"
+}
