28b16507ff5c0d3cf7fa83c1a90572b0de003d65
Rohit R <47154150+rohit7887@users.noreply.github.com>
2021-12-10 11:29:27 +0530

    Update launchinstance.tf

--- launchinstance.tf
+++ launchinstance.tf
@@ -5,8 +5,10 @@ provider "aws" {
 
 
 resource "aws_instance" "web" {
-  ami           = "ami-0dd0ccab7e2801812"
-  instance_type = "t2.micro"
+  ami               = var.ami_id
+  instance_type     = "t2.micro"
+  key_name          = var.key_name
+  vpc_security_group_ids = [var.sg_id]
   tags = {
     Name = "Launched-via-Terraform"
   }
