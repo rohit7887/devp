6aaf345d7744ef2aee68c891bc54a92e7378aad9
Rohit R <47154150+rohit7887@users.noreply.github.com>
2021-12-10 16:10:04 +0530

    Update launchinstance.tf

--- launchinstance.tf
+++ launchinstance.tf
@@ -1,5 +1,5 @@
 provider "aws" {
- region = "us-east-1"
+ region = "us-east-2"
  access_key= "AKIA3VUJSLOXGGJLIR5U"
   secret_key= "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"
 }
@@ -7,7 +7,7 @@ provider "aws" {
 
 
 resource "aws_instance" "web" {
-  ami           = "ami-083654bd07b5da81d"
+  ami           = "ami-0dd0ccab7e2801812"
   instance_type     = "t2.micro"
   key_name          = var.key_name
   vpc_security_group_ids = [var.sg_id]
